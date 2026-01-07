45a99acead5e40a101e8dcfa52759508627223f4
DESKTOP-HGOU9K1\MohamedHammad <mohamed.tony@fcai.usc.edu.eg>
2026-01-07 21:24:47 +0200

    trying soruce tree

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World tony!'))),
+      home: Scaffold(body: Center(child: Text('Hello World tony hammad!'))),
     );
   }
 }
