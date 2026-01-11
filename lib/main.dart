d754762f1f18b80521085ebe83723a05f015d702
DESKTOP-HGOU9K1\MohamedHammad <mohamed.tony@fcai.usc.edu.eg>
2026-01-11 16:13:10 +0200

    - fixing splash screen for android 12 +

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/core/routing/app_router.dart';
 import 'package:flutter_advanced_course/doc_doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(DocAPP(appRouter: AppRouter()));
 }
